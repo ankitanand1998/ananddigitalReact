62de195881fbc255c0e60bff5055bd33f77a88b6
ankitanand1998 <ankitanand1998find@gmail.com>
2024-12-30 23:04:50 +0530

    Update BlogDetail.tsx

--- src/components/BlogDetail.tsx
+++ src/components/BlogDetail.tsx
@@ -38,7 +38,7 @@ export const BlogDetail: React.FC<BlogDetailProps> = ({ post }) => {
 
         <section className="mb-8">
           <h2 className="text-2xl font-bold mb-4">{post.detailedHeadingSection2}</h2>
-           {post.paragraph2}
+           dangerouslySetInnerHTML={{ __html: post.paragraph2 }}
         </section>
 
         {post.middleImg && (
