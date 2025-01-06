6859c26d694a51768afe6b3f3fe25bec5ce09a26
ankitanand1998 <ankitanand1998find@gmail.com>
2025-01-06 20:40:29 +0530

    Update blogService.ts

--- src/services/blogService.ts
+++ src/services/blogService.ts
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=hY_0bKP4jMkl2hPUuMnm5rGaS-Xu-ygDuIkn6wbXrRf2mOrJPu0M6VROwzNKuo98Se-UWh3BbEe9AxkVgXIvXrfXb_P79Osdm5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnIONs2Ox7_Es2K09SRvyoRiiydX-tP1Bc_ckb5b5oXaNcSt-MBdsQVux_MGmO2ItVlgrLbxZkFEu-ctXRjCDfbc4rRxWkWgpVtz9Jw9Md8uu&lib=M7cSQCE0KZWQ8JoD5XuMfI4Dr_WGpAQrE';
+const API_URL = 'data.json';
 
 export const fetchBlogPosts = async () => {
   try {
