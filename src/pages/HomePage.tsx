179a1adc1f30c56adadb50abb0d2f107c7e18ad4
ankitanand1998 <ankitanand1998find@gmail.com>
2025-01-07 23:19:10 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -48,8 +48,8 @@ export const HomePage: React.FC = () => {
   return (
     <>
       <SEO 
-        title=">Anand Digital Blog:Breaking News,Top Headlines & Insights Across India"
-        description="Get the latest breaking news on politics, business, lifestyle, entertainment and sports.Stay updated with global news, live coverage on Bollywood, cricket, technology, and more on Anand Digital Blog."
+        title="Anand Digital Blog:Breaking News,Top Headlines & Insights Across India"
+        description="Get the latest breaking news on politics, business, lifestyle, entertainment and sports.Stay updated with global news, live coverage on Bollywood, cricket, technology and more on Anand Digital Blog."
         canonicalUrl="https://ananddigitalblog.vercel.app/"
       />
       <script type="application/ld+json">
