872693b0c0259196e08a1b5b06563323f5d0b019
ankitanand1998 <ankitanand1998find@gmail.com>
2025-02-21 22:24:22 +0530

    Update HeroSlider.tsx

--- src/components/hero/HeroSlider.tsx
+++ src/components/hero/HeroSlider.tsx
@@ -5,22 +5,22 @@ const slides = [
   {
     id: 1,
     image: 'https://images.unsplash.com/photo-1504711434969-e33886168f5c?q=80&w=1920&h=600&fit=crop',
-    title: 'Decoding the Political Landscape: Insights That Matter',
-    description: 'Explore the intricate world of politics with in-depth analysis, expert commentary, and real-time updates. We break down complex issues to keep you informed and empowered',
+    title: 'Skyrocket Your Website with SEO & Digital Mastery',
+    description: 'Enhance your website’s visibility and traffic with top-tier SEO and digital marketing tactics. Master the tools and trends you need to dominate search rankings at Anand Digital Blog',
     link: '/about'
   },
   {
     id: 2,
     image: 'https://images.unsplash.com/photo-1517048676732-d65bc937f952?q=80&w=1920&h=600&fit=crop',
-    title: 'The Intersection of Tech and Humanity',
-    description: 'Explore how technology is bridging gaps, creating opportunities, and redefining the human experience. Stay inspired by the possibilities',
+    title: 'Boost Your Digital Edge with Expert Insights',
+    description: 'Gain a competitive advantage with proven strategies in SEO, content writing, and web development. Stay ahead of trends and elevate your skills with expert advice from Anand Digital Blog',
     link: '/about'
   },
   {
     id: 3,
     image: 'https://images.unsplash.com/photo-1495020689067-958852a7765e?q=80&w=1920&h=600&fit=crop',
-    title: 'Rank Higher, Reach Further: SEO Tips That Work',
-    description: 'Learn the secrets to achieving top search engine rankings. Our blog provides the insights you need to grow your online presence and drive organic traffic.',
+    title: 'Master Corporate Culture in a Digital World',
+    description: 'Know to navigate workplace dynamics and excel in a fast-paced digital environment. From remote teams to corporate pressures, get tips to thrive as a digital marketing or tech professiona',
     link: '/about'
   }
 ];
