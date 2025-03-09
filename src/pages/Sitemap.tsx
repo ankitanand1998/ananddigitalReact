2db6a02536bff30e1fe3fb5378aeed24a85ae7f8
ankitanand1998 <ankitanand1998find@gmail.com>
2025-03-09 14:12:34 +0530

    Update Sitemap.tsx

--- src/pages/Sitemap.tsx
+++ src/pages/Sitemap.tsx
@@ -5,27 +5,47 @@ export const Sitemap: React.FC = () => {
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   <url>
     <loc>https://ananddigitalblog.vercel.app/</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
+   <lastmod>2025-03-09T08:31:01.920Z</lastmod>
     <priority>1.0</priority>
   </url>
-  <url>
-    <loc>https://ananddigitalblog.vercel.app/manmohan-singh-oasis-of-integrity-in-sea-of-opportunism</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
-    <priority>0.8</priority>
-  </url>
+ 
 
   <url>
     <loc>https://ananddigitalblog.vercel.app/oppo-find-x8-series-design-and-features</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
+    <lastmod>2025-03-09T08:31:01.920Z</lastmod>
     <priority>0.8</priority>
   </url>
 
   <url>
     <loc>https://ananddigitalblog.vercel.app/how-to-start-your-personal-blog</loc>
-    <lastmod>${new Date().toISOString()}</lastmod>
+    <lastmod>2025-03-09T08:31:01.920Z</lastmod>
     <priority>0.8</priority>
   </url>
- </urlset>`;
+
+ <url>
+    <loc>https://ananddigitalblog.vercel.app/top-job-red-flags-before-accepting-offer</loc>
+    <lastmod>2025-03-09T08:31:01.920Z</lastmod>
+    <priority>0.8</priority>
+  </url>
+  
+<url>
+    <loc>https://ananddigitalblog.vercel.app/how-to-save-yourself-from-workplace-politics</loc>
+    <lastmod>2025-03-09T08:31:01.920Z</lastmod>
+    <priority>0.8</priority>
+  </url>
+
+  <url>
+    <loc>https://ananddigitalblog.vercel.app/football-match-punjab-fc-vs-northeast-united</loc>
+    <lastmod>2025-03-09T08:31:01.920Z</lastmod>
+    <priority>0.8</priority>
+  </url>
+
+   <url>
+    <loc>https://ananddigitalblog.vercel.app/manmohan-singh-oasis-of-integrity-in-sea-of-opportunism</loc>
+   <lastmod>2025-03-09T08:31:01.920Z</lastmod>
+    <priority>0.8</priority>
+  </url>
+</urlset>`;
     
     const blob = new Blob([xmlData], { type: 'application/xml' });
     const url = URL.createObjectURL(blob);
