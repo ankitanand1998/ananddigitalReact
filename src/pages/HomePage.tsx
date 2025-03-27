177dd0db2e5123a9c631217832e94ea4d5b13cd1
ankitanand1998 <ankitanand1998find@gmail.com>
2025-03-27 20:27:31 +0530

    Update HomePage.tsx

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -93,9 +93,8 @@ export const HomePage: React.FC = () => {
             <p classNameName="px-3">Welcome to Anand Digital Blog, your go-to resource for SEO, digital marketing, and website optimization. Whether you're looking to rank higher on Google, grow organic traffic, or enhance your online presence, we provide expert insights and professional services to help you succeed.
  Explore our in-depth blogs packed with proven SEO strategies, content marketing techniques, and website development tips to keep you ahead of the competition. Learn how to create high-impact blogs with our step-by-step guide on <a href="https://ananddigitalblog.vercel.app/how-to-start-your-personal-blog" classNameName="custom-link">writing engaging content</a> that attracts both readers and search engines.
  At Anand Digital Blog, we don’t just share knowledge—we offer expert services to help businesses dominate search rankings and drive conversions.</p> 
-   <div className="container mt-5 our_services">
-    <div className="row justify-content-center">
-        <div className="col-md-6">
+  
+        <div className="our_services">
             <div className="services-card">
                 <h4 className="text-center mb-4">Our Services</h4>
                 <div className="service-item"><i className="fas fa-search"></i>SEO Services – Keyword research, on-page & off-page SEO, and technical optimization.</div>
@@ -105,8 +104,8 @@ export const HomePage: React.FC = () => {
                 <div className="service-item"><i className="fas fa-code"></i>Website Development – SEO-friendly websites that enhance user experience and ranking.</div>
             </div>
         </div>
-    </div>
-</div>
+    
+
 <p classNameName="px-3">Stay updated with cutting-edge SEO and digital marketing strategies  that help businesses achieve online success. Whether you're a blogger, entrepreneur, or business owner, our blogs and services provide everything you need to grow your online presence effectively
  Start exploring now and let us help you skyrocket your SEO success!</p> 
 <h2 classNameName="px-3">Check Out Our Latest Blogs</h2>
