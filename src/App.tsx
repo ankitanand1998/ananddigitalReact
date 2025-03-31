da6afe6a21699648eb33912559ee67534aa73816
ankitanand1998 <ankitanand1998find@gmail.com>
2025-03-31 10:55:45 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { Navbar } from "./components/navbar/Navbar";
 import { HomePage } from './pages/HomePage';
 import { BlogDetailPage } from './pages/BlogDetailPage';
 import { AboutPage } from './pages/AboutPage';
-// import { Sitemap } from "./pages/Sitemap";
+import { Sitemap } from "./pages/Sitemap";
 import { RobotsTxt } from "./pages/RobotsTxt";
 import {CategoryPage} from "./pages/CategoryPage";
 
@@ -22,7 +22,7 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/about" element={<AboutPage />} />
             <Route path="/:url" element={<BlogDetailPage />} />
-{/*             <Route path="/sitemap.xml" element={<Sitemap />} /> */}
+            <Route path="/sitemap.xml" element={<Sitemap />} /> 
             <Route path="/robots.txt" element={<RobotsTxt />} />
             <Route path="/category/:categoryName" element={<CategoryPage />} />
            
