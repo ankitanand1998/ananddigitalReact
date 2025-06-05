a754588d87552cce8fbc410f82a004c5d6beccf3
ankitanand1998 <ankitanand1998find@gmail.com>
2025-06-05 22:02:57 +0530

    Update Services.tsx

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -115,7 +115,7 @@ const Services = () => {
   return (
     <div className="pt-16">
      <Helmet>
-            <title>Digital Marketing & PR Services in Delhi & Noida ,India | Anand Digital PR</title>
+            <title>Digital PR Agency in Delhi Noida | Anand Digital PR</title>
             <meta name="description" content="Looking for expert PR and digital marketing services in Delhi NCR & Noida,India ? Anand Digital PR offers SEO, brand strategy, content marketing, media coverage, and reputation management tailored for growth." />
             <link rel="canonical" href="https://www.ananddigitalpr.com/services" />
             <meta name="robots" content="index, follow" />
