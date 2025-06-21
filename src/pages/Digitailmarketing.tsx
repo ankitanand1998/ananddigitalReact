e68a207265b9764e1f0ac643a559793de18cd035
Anand Digital PR <ankitanand1998find@gmail.com>
2025-06-21 23:08:19 +0530

    Update Digitailmarketing.tsx

--- src/pages/Digitailmarketing.tsx
+++ src/pages/Digitailmarketing.tsx
@@ -4,6 +4,12 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Autoplay } from 'swiper/modules';
 import { Helmet } from 'react-helmet';
 import Footer from "../components/Footer";
+import WhyChoose from "../components/WhyChoose";
+import  About from "./About"
+import  Contact from "./Contact"
+import ClientReviews1 from '../Images/Client-Reviews-Erica-Koomsom.png';
+import ClientReviews2 from '../Images/Client-Reviews-Nishant-Kumar.png';
+import ClientReviews3 from '../Images/Client-Reviews-sk-saifuddin.png';
 import {
    TrendingUp,
   BarChart2,
