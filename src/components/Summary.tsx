8da1331bc0feba2fb5a4c1ac5b403046c2ffc5d6
Anand Digital PR <ankitanand1998find@gmail.com>
2025-07-11 23:24:44 +0530

    Update Summary.tsx

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -11,12 +11,12 @@ const Summary = () => {
          <b>This blog is brought to you by Anand Digital PR</b> one of India’s 
           <span><a href="https://www.ananddigitalpr.com/" className="text-blue-600 hover:underline"> leading Digital Marketing and PR agencies</a>. With years of hands-on experience, our team is passionate about helping brands grow through smart, creative, and results-driven strategies. From boosting your online presence to building lasting customer trust, we know what it takes to succeed in the digital world. At Anand Digital PR, we don’t just follow trends — we help set them. This blog is part of our mission to share useful insights, real-world tips, and honest advice to help businesses like yours thrive online. </span></p><br/><br/>
             <div  className="bg-white rounded-2xl shadow-md p-6 border border-gray-200 text-center">
-              <b>Join our community on reddit</b> <br/>
+              <b>Join our community on reddit</b> <br/> 
              <a 
   href="https://www.reddit.com/r/DigitalandPR/s/a7U8nJpiKY" 
   target="_blank" 
   rel="noopener noreferrer" 
-  className="inline-block bg-red-600 text-white px-4 py-2 rounded-lg hover:bg-red-700 transition"
+  className="inline-block bg-red-600 text-white px-4 py-4 rounded-lg hover:bg-red-700 transition"
 >
   Join Us on Reddit
 </a>
