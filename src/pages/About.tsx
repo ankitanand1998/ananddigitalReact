f2dff4719b964253941f3a7c83fad0f4a721dc79
Anand Digital PR <ankitanand1998find@gmail.com>
2025-07-18 20:50:57 +0530

    Update About.tsx

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -22,15 +22,15 @@ const About = () => {
 
   const team = [
     {
-      name: 'Ankit Anand',
+      name: 'Aniket Kumar',
       role: 'Co-Founder',
-      image: 'https://scontent.fdel5-3.fna.fbcdn.net/v/t39.30808-6/461502543_2277995139225610_5857932059119235261_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=bKoZFocn_uIQ7kNvwG_O0IN&_nc_oc=AdkDu_DT9TreqVKBmJuwzgJLGSx5wQL1y5p5sZmddQ-j1cEjwW-lhEhA1DAaOKCYwTA&_nc_zt=23&_nc_ht=scontent.fdel5-3.fna&_nc_gid=vOB1Yq4ooeUN3CaB4JCP1g&oh=00_AfEulrDVV516-9zpEHvH--w16qreTRcEoDCkMZ9en19bxA&oe=68080466',
+      image: '',
       bio: 'Experienced digital marketer and PR professional with a strong background in driving brand growth and visibility.',
     },
     {
       name: 'Vikram Mahto',
       role: 'Co-Founder',
-      image: 'https://scontent.fdel5-3.fna.fbcdn.net/v/t39.30808-6/474912556_2288722418172041_8204393816926142506_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=6ee11a&_nc_ohc=gq2I1XvHf2UQ7kNvwEKn5Pn&_nc_oc=AdnoCg6WKvs3L7aR5cY4ZXiI0ARsapNHrt21p8Xf-VEw9P3I9Di1h-hiv4wjSbPuWfo&_nc_zt=23&_nc_ht=scontent.fdel5-3.fna&_nc_gid=UtmcmAKIesItIgTJYAsQjg&oh=00_AfEOmxnFVWV4BddNN7Mv2W-lG_eI6k5yDePWdef6Lw6Dqg&oe=6808036D',
+      image: '',
       bio: 'Experienced Software Developer with a strong background in designing, developing, and deploying high-performance applications, focused on clean code, scalability, and innovation',
     },
     {
